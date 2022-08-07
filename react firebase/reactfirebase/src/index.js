2852f645a203b40573e27498b2d57374ae473a82
bearlikecode <bearlikecode@gmail.com>
2022-08-07 17:32:40 +0300

    Updated folder structure, added firebase package and firebase config

--- react firebase/reactfirebase/src/index.js
+++ react firebase/reactfirebase/src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { ReactReduxFirebaseProvider } from "react-redux-firebase";
+import firebase from 'firebase/app';
 import './index.css';
 import App from './App';
+import { store } from './store/index';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rrfProps = {
+  firebase,
+  config: {
+    userProfile: "users" 
+  },
+  dispatch: store.dispatch
+};
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </ReactReduxFirebaseProvider>
+    </Provider>
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
