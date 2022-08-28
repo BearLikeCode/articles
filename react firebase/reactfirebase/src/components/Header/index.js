42d7f0debf52dd93808fb7546a6ab36d20dfe9bd
bearlikecode <bearlikecode>
2022-08-28 14:30:56 +0300

    Updated header component

--- react firebase/reactfirebase/src/components/Header/index.js
+++ react firebase/reactfirebase/src/components/Header/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './styles.css'
 
 export const Header = () => {
     return (
         <div className='header'>
             <div className='header-wrapper'>
-                <a href="#">Create post</a>
+                <div className='header-menu'>
+                    <Link to="/">Articles</Link>
+                    <Link to="/new">Create post</Link>
+                </div>
             </div>
         </div>
     )
